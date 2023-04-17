bc20d789912cfe636d08c631cbd6380f3a7658c7
Hector Brown <hectorjbrown@protonmail.com>
2023-04-17 02:48:08 +0100

    improvements to pend

--- content/phys/pend.md
+++ content/phys/pend.md
@@ -7,3 +7,63 @@ draft: false
 <div id="pend-parent" style="width: 100%; height: auto;"></div>
 <script src="https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/p5.js"></script> <!-- load p5.js from CDN--> 
 <script src="/scripts/phys/pend/pend.js"></script>
+
+<style>
+    #control-panel {
+        display: grid;
+        grid-row-gap: 0.5rem;
+        grid-template-columns: 3fr 1fr;
+        grid-column-gap: 1rem;
+    }
+    @media (max-width: 800px) {
+        #control-panel {
+            grid-template-columns: 2fr 1fr;
+        }
+    }
+    @media (max-width: 500px) {
+        #control-panel {
+            grid-template-columns: 1fr;
+        }
+    }
+</style>
+
+<div id="control-panel">
+    <div>
+    <div style="margin: 1vw">
+        <input type="range" min="-1" max="2" value="0" step="0.01" class="slider" id="m1"></input>
+        <p>\( m_1 \): <span id="m1_out"></span></p>
+    </div>
+    <div style="margin: 1vw">
+        <input type="range" min="-1" max="2" value="0" step="0.01" class="slider" id="m2"></input>
+        <p>\( m_2 \): <span id="m2_out"></span></p>
+    </div>
+    <div style="margin: 1vw">
+        <input type="range" min="0.1" max="1" value="0.5" step="0.01" class="slider" id="l1"></input>
+        <p>\( l_1 \): <span id="l1_out"></span></p>
+    </div>
+    <div style="margin: 1vw">
+        <input type="range" min="0.1" max="1" value="0.5" step="0.01" class="slider" id="l2"></input>
+        <p>\( l_2 \): <span id="l2_out"></span></p>
+    </div>
+    <div style="margin: 1vw">
+        <input type="range" min="-3.14" max="3.14" value="2" step="0.01" class="slider" id="theta_0"></input>
+        <p>\( \theta_0 \): <span id="theta_0_out"></span></p>
+    </div>
+    <div style="margin: 1vw">
+        <input type="range" min="-3.14" max="3.14" value="0" step="0.01" class="slider" id="phi_0"></input>
+        <p>\( \phi_0 \): <span id="phi_0_out"></span></p>
+    </div>
+    <div style="margin: 1vw">
+        <input type="range" min="-1" max="2" value="1" step="0.01" class="slider" id="g"></input>
+        <p>\( g \): <span id="g_out"></span></p>
+    </div>
+    </div>
+    <div>
+        <p>\( \theta \): <span id="theta"></span></p>
+        <p>\( \phi \): <span id="phi"></span></p>
+        <p>\( \frac{d\theta}{dt} \): <span id="alpha"></span></p>
+        <p>\( \frac{d\phi}{dt} \): <span id="beta"></span></p>
+        <p>\( T \): <span id="T"></span></p>
+        <p>\( V \): <span id="V"></span></p>
+        <label class="checkbox">Trace: <input type="checkbox" checked="checked" id="trace"></input><span class="checkmark"></span></label>
+</div>
--- static/scripts/phys/pend/pend.js
+++ static/scripts/phys/pend/pend.js
@@ -14,6 +14,12 @@ let pend = new p5((sketch) => {
 
   sketch.windowResized = () => {
     sketch.resizeCanvas(parent.clientWidth, parent.clientWidth);
+    anchor = sketch.createVector(sketch.width / 2, sketch.height / 5);
+    tracebuffer = [];
+  };
+
+  sketch.sigmoid = (scale, spread, x_0, x) => {
+    return scale / (1 + Math.E ** (-spread * (x - x_0)));
   };
 
   sketch.get_sysvars = (state) => {
@@ -40,6 +46,21 @@ let pend = new p5((sketch) => {
     return output;
   };
 
+  sketch.set_U0 = () => {
+    T =
+      (1 / 2) * m1 * l1 ** 2 * state[2] ** 2 +
+      (1 / 2) *
+        m2 *
+        (l1 * state[2] + l2 * state[3] * sketch.cos(state[1])) ** 2 +
+      (1 / 2) * m2 * (l2 * state[3] * sketch.sin(state[1])) ** 2;
+    V =
+      -m1 * g * l1 * sketch.cos(state[0]) -
+      m2 *
+        g *
+        (l1 * sketch.cos(state[0]) + l2 * sketch.cos(state[0] + state[1]));
+    U_0 = T + V;
+  };
+
   sketch.setup = () => {
     parent = document.getElementById("pend-parent");
     canvas = sketch.createCanvas(parent.clientWidth, parent.clientWidth);
@@ -54,7 +75,8 @@ let pend = new p5((sketch) => {
     anchor = sketch.createVector(sketch.width / 2, sketch.height / 5);
 
     //theta, phi, alpha, beta
-    state = [2, 0, 0, 0];
+    state_0 = [2, 0, 0, 0];
+    state = state_0.slice();
     system = [
       (state) => {
         return state[2];
@@ -79,6 +101,63 @@ let pend = new p5((sketch) => {
     ];
 
     tracebuffer = [];
+
+    sketch.set_U0();
+
+    {
+      document.getElementById("m1_out").innerHTML = m1.toFixed(3);
+      document.getElementById("m1").oninput = () => {
+        m1 = 10 ** document.getElementById("m1").value;
+        document.getElementById("m1_out").innerHTML = m1.toFixed(3);
+        sketch.set_U0();
+      };
+      document.getElementById("m2_out").innerHTML = m2.toFixed(3);
+      document.getElementById("m2").oninput = () => {
+        m2 = 10 ** document.getElementById("m2").value;
+        document.getElementById("m2_out").innerHTML = m2.toFixed(3);
+        sketch.set_U0();
+      };
+      document.getElementById("l1_out").innerHTML = l1.toFixed(3);
+      document.getElementById("l1").oninput = () => {
+        l1 = Number(document.getElementById("l1").value);
+        l1_pix = (l1 / 1.5) * sketch.height;
+        document.getElementById("l1_out").innerHTML = l1.toFixed(3);
+        sketch.set_U0();
+      };
+      document.getElementById("l2_out").innerHTML = l2.toFixed(3);
+      document.getElementById("l2").oninput = () => {
+        l2 = Number(document.getElementById("l2").value);
+        l2_pix = (l2 / 1.5) * sketch.height;
+        document.getElementById("l2_out").innerHTML = l2.toFixed(3);
+        sketch.set_U0();
+      };
+      document.getElementById("theta_0_out").innerHTML = state_0[0].toFixed(3);
+      document.getElementById("theta_0").oninput = () => {
+        state_0[0] = Number(document.getElementById("theta_0").value);
+        document.getElementById("theta_0_out").innerHTML =
+          state_0[0].toFixed(3);
+        state = state_0.slice();
+        tracebuffer = [];
+        sketch.set_U0();
+      };
+      document.getElementById("g_out").innerHTML = g.toFixed(3);
+      document.getElementById("g").oninput = () => {
+        g = 10 ** document.getElementById("g").value;
+        document.getElementById("g_out").innerHTML = g.toFixed(3);
+        sketch.set_U0();
+      };
+      document.getElementById("phi_0_out").innerHTML = state_0[1].toFixed(3);
+      document.getElementById("phi_0").oninput = () => {
+        state_0[1] = Number(document.getElementById("phi_0").value);
+        document.getElementById("phi_0_out").innerHTML = state_0[1].toFixed(3);
+        state = state_0.slice();
+        tracebuffer = [];
+        sketch.set_U0();
+      };
+      document.getElementById("trace").oninput = () => {
+        tracebuffer = [];
+      };
+    }
   };
 
   sketch.draw = () => {
@@ -98,38 +177,51 @@ let pend = new p5((sketch) => {
     );
 
     sketch.strokeWeight(2);
-    tracebuffer.unshift([bob2.x, bob2.y]);
-    console.log(tracebuffer);
-    sketch.noFill();
-    for (let i = 0; i <= Math.ceil(tracebuffer.length / 10); i++) {
-      sketch.stroke(131, 165, 152, (255 * (50 - i)) / 50);
-      sketch.beginShape();
-      var list_start;
-      if (i == 0) {
-        list_start = 0;
-      } else {
-        list_start = i * 10 - 1;
+    if (document.getElementById("trace").checked) {
+      tracebuffer.unshift([bob2.x, bob2.y]);
+      sketch.noFill();
+      for (let i = 0; i <= Math.ceil(tracebuffer.length / 10); i++) {
+        sketch.stroke(131, 165, 152, (255 * (50 - i)) / 50);
+        sketch.beginShape();
+        var list_start;
+        if (i == 0) {
+          list_start = 0;
+        } else {
+          list_start = i * 10 - 1;
+        }
+        tracebuffer.slice(list_start, (i + 1) * 10 + 2).forEach((element) => {
+          sketch.curveVertex(element[0], element[1]);
+        });
+        sketch.endShape();
       }
-      tracebuffer.slice(list_start, (i + 1) * 10 + 2).forEach((element) => {
-        sketch.curveVertex(element[0], element[1]);
-      });
-      sketch.endShape();
-    }
-    if (tracebuffer.length > 500) {
-      tracebuffer.pop();
+      if (tracebuffer.length > 500) {
+        tracebuffer.pop();
+      }
     }
     sketch.fill(213, 196, 161);
     sketch.stroke(213, 196, 161);
     sketch.strokeWeight(3);
     sketch.line(anchor.x, anchor.y, bob1.x, bob1.y);
     sketch.line(bob1.x, bob1.y, bob2.x, bob2.y);
-    sketch.circle(bob1.x, bob1.y, 30);
-    sketch.circle(bob2.x, bob2.y, 30);
+    sketch.circle(bob1.x, bob1.y, sketch.sigmoid(sketch.width / 10, 2, 1, m1));
+    sketch.circle(bob2.x, bob2.y, sketch.sigmoid(sketch.width / 10, 2, 1, m2));
     sketch.stroke(102, 92, 84);
     sketch.fill(102, 92, 84);
     sketch.circle(anchor.x, anchor.y, 20);
 
     state = sketch.rk4(state, system, sketch.deltaTime / 1000);
+    while (state[0] > sketch.PI) {
+      state[0] -= 2 * sketch.PI;
+    }
+    while (state[0] < -sketch.PI) {
+      state[0] += 2 * sketch.PI;
+    }
+    while (state[1] > sketch.PI) {
+      state[1] -= 2 * sketch.PI;
+    }
+    while (state[1] < -sketch.PI) {
+      state[1] += 2 * sketch.PI;
+    }
     T =
       (1 / 2) * m1 * l1 ** 2 * state[2] ** 2 +
       (1 / 2) *
@@ -141,10 +233,20 @@ let pend = new p5((sketch) => {
       m2 *
         g *
         (l1 * sketch.cos(state[0]) + l2 * sketch.cos(state[0] + state[1]));
-    console.log("T: ", T);
-    console.log("V: ", V);
-    console.log("U: ", T + V);
-    console.log(state);
+    if (Math.abs(T + V) > 1.1 * Math.abs(U_0)) {
+      state = state_0.slice();
+      tracebuffer = [];
+      console.log("Energy exceeded initial value, resetting to avoid crash.");
+      sketch.set_U0();
+    }
+
+    //display vars
+    document.getElementById("theta").innerHTML = state[0].toFixed(3);
+    document.getElementById("phi").innerHTML = state[1].toFixed(3);
+    document.getElementById("alpha").innerHTML = state[2].toFixed(3);
+    document.getElementById("beta").innerHTML = state[3].toFixed(3);
+    document.getElementById("T").innerHTML = T.toFixed(3);
+    document.getElementById("V").innerHTML = V.toFixed(3);
   };
 
   sketch.rk4 = (state, system, dt) => {
