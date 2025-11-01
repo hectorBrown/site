03f6810c57baa3b67832255293472827f558f77a
Hector Brown <hectorjbrown@protonmail.com>
2025-11-01 13:57:12 +1100

    build tools to optimize rust before deployment + optimized index background

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 rm -r public/
+
+root=$(pwd)
+for dir in rust/*; do
+	cd "$dir" || exit
+	./build.sh --release
+done
+cd "$root" || exit
 hugo && rsync -avh public/ pi:/home/hex/site/content --delete
--- rust/index-background/build.sh
+++ rust/index-background/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-
-cargo build --release
-cp target/wasm32-unknown-unknown/release/index-background.wasm ../../static/wasm/index-background.wasm
+if [[ $1 == "--release" ]]; then
+	cargo build --release
+	wasm-opt -Oz -o ../../static/wasm/index-background.wasm target/wasm32-unknown-unknown/release/index-background.wasm
+else
+	cargo build
+	cp target/wasm32-unknown-unknown/debug/index-background.wasm ../../static/wasm/index-background.wasm
+fi
