316910319d70b237774afc7fa68d4f3629e6ed12
Hector Brown <hectorjbrown@protonmail.com>
2025-11-04 16:20:24 +1100

    working wgpu drawing triangle with shader

--- rust/index-background/build.sh
+++ rust/index-background/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+cd ..
 if [[ $1 == "--release" ]]; then
-	cargo build --release
-	wasm-opt -Oz -o ../../static/wasm/index-background.wasm target/wasm32-unknown-unknown/release/index-background.wasm
+	wasm-pack build --target web --release index-background
 else
-	cargo build
-	cp target/wasm32-unknown-unknown/debug/index-background.wasm ../../static/wasm/index-background.wasm
+	wasm-pack build --target web index-background
 fi
+cd index-background || exit 1
+rm -r ../../static/wasm/index-background/
+cp -r pkg ../../static/wasm/index-background/
