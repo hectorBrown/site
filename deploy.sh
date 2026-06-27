fafdd1b5aa9429250835006f6e074f0c2aac6490
Hector Brown <hectorjbrown@protonmail.com>
2026-06-27 13:56:09 +1000

    don't build rust on every deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 rm -r public/
 
-root=$(pwd)
-for dir in rust/*; do
-  cd "$dir" || exit
-  ./build.sh --release
-done
-cd "$root" || exit
 hugo && rsync -avh public/ pi-01:/home/hex/site/content --delete
